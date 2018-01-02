3d07bb997666406901da0901017b8920936e8869
samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2018-01-02 14:16:33 -0500

    updated reducers to fetch values from node server

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,13 @@
 import React from 'react';
+var axios = require('axios');
 
 export default function() {
+  
+  var domain = 'http://localhost:3000/pipelines/';
+  var data = axios.get(domain, { crossdomain: true }).then(function(data){
+        console.log(data);
+        return data
+  });
   return [
     { title: 'Javascript: The good parts', pages: 30},
     { title: 'Harry Potter', pages: 20},
