eb2a91bea2ddd7a647318ded416c3e40504813e6
samvarankashyap <samvaran.kashyap@gmail.com>
2018-02-06 15:36:41 -0500

    Added REST API for runview with rundata(needs node data too)

--- node_routes/routes.js
+++ node_routes/routes.js
@@ -1,6 +1,9 @@
 var axios = require('axios')
 const util = require('util');
+const sentry = require('raven');
+
 const ROOT_URL = "https://jenkins-continuous-infra.apps.ci.centos.org/blue/rest/organizations/jenkins/pipelines/";
+const JENKINS_URL = "https://jenkins-continuous-infra.apps.ci.centos.org";
 function getPipelines(url){
   return axios.get(ROOT_URL, { responseType: 'json' });
 }
@@ -13,11 +16,16 @@ function getPipelineRuns(name){
   return axios.get(ROOT_URL+name+'/runs/', { responseType: 'json' });
 }
 
+function getPipelineRunview(name){
+  return axios.get(ROOT_URL+name+'/runs/', { responseType: 'json' });
+}
+
 function getPipelineRunByID(name, runid){
   return axios.get(ROOT_URL+name+'/runs/'+runid, { responseType: 'json' });
 }
 
 function getPipelineRunNodes(name, runid){
+  console.log(ROOT_URL+name+'/runs/'+runid+'/nodes/');
   return axios.get(ROOT_URL+name+'/runs/'+runid+'/nodes/', { responseType: 'json' });
 }
 
@@ -37,6 +45,36 @@ var appRouter = function (app) {
     var promiseObj = getPipelineRunByID(req.params.id, req.params.runid);
     promiseObj.then(function(data){
       res.status(200).send(data['data']);
+    });
+  });
+
+  app.get("/pipelines/:id/runview", function(req, res) {
+    var promiseObj = getPipelineRunview(req.params.id);
+    run_data = {}
+    run_data["node_promises"] = []
+    run_data["node_results"] = []
+    var return_data = []
+    promiseObj.then(function(data){
+      ;
+      console.log("Start of console : runview")
+      //console.log(data);
+      for (run_index in data['data']){
+        var append_dict = {}
+        append_dict["name"]= data['data'][run_index]['name'];
+        append_dict["runid"]= data['data'][run_index]['id'];
+        append_dict["pipeline"]= data['data'][run_index]['pipeline'];
+        append_dict["durationInMillis"]= data['data'][run_index]['durationInMillis'];
+        run_data["node_promises"].push(getPipelineRunNodes(append_dict["pipeline"], append_dict["runid"]));
+        return_data.push(append_dict);
+      }
+      console.log(run_data);
+      return return_data;
+
+    }).then((response) => {
+      axios.all(run_data["node_promises"]).then(function(values) {
+        console.log(values);
+      });
+      res.status(200).send(response);
     });
   });
 
