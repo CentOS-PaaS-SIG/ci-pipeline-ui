7ae5bb8aaf6c6152e2040604d4bffd7a216d4a27
samvaran kashyap rallabandi <srallaba@redhat.com>
2018-04-25 15:43:24 -0400

    Rendered steps and nodes on to pipeline timeline view

--- src/components/clpsb.js
+++ src/components/clpsb.js
@@ -3,11 +3,45 @@ import { connect } from 'react-redux';
 import Collapsible from 'react-collapsible';
 import Test from './test';
 var CONFIG = require("../constants/config")
+import { fetchPipelineRunNodeSteps }  from '../actions';
 
 
 class Clpsb extends Component {
 
+  shouldComponentUpdate(nextProps, nextState){
+    if(this.props.nodeId != nextProps.nodeId ){
+      let pipelineName = nextProps.pipelineName
+      let runId = nextProps.runId;
+      let nodeId = nextProps.nodeId;
+      this.props.fetchPipelineRunNodeSteps(pipelineName, runId, nodeId);
+      return true;
+    }
+    return true
+  }
+
+  renderSteps(){
+    return _.map(this.props.pipelines.pipelinenodesteps, step => {
+      //onsole.log(step);
+      return(
+        <Collapsible key={step.id} trigger={step.displayName}>
+          <p>This is the collapsible content. It can be any element or React component you like.</p>
+          <p>It can even be another Collapsible component. Check out the next section!</p>
+        </Collapsible>
+      );
+    });
+  }
+
   render(){
+      //console.log("Inside render");
+      //console.log(this.props.pipelines);
+      if (this.props.pipelines.pipelinenodesteps){
+        return(
+          <div>
+            {this.renderSteps()}
+          </div>
+        );
+      }
+      else{
       return (
       <div>
         <Collapsible trigger="something">
@@ -20,6 +54,12 @@ class Clpsb extends Component {
       </div>
       );
     }
+    }
 }
 
-export default Clpsb;
+
+function mapStateToProps(state){
+  return {pipelines: state.pipelines};
+}
+
+export default connect(mapStateToProps, { fetchPipelineRunNodeSteps })(Clpsb);
--- src/components/pipeline_timeline.js
+++ src/components/pipeline_timeline.js
@@ -9,19 +9,64 @@ var CONFIG = require("../constants/config")
 
 
 class PipelineTimeline extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        'pipelineName': this.props.match.params.id,
+        'runId': this.props.match.params.runid,
+      }
+    this.testClick = this.testClick.bind(this);
+  }
   componentWillMount() {
-    console.log(this.props.match.params.id);
+    //console.log(this.props.match.params.id);
     let pipelineName = this.props.match.params.id;
     let runid = this.props.match.params.runid;
     this.props.fetchPipelineRunNodes(pipelineName, runid);
   }
+
+  testClick(nodeId){
+    console.log("clicked");
+    this.setState({
+        'pipelineName': this.props.match.params.id,
+        'runId': this.props.match.params.runid,
+        'nodeId': nodeId
+      }
+    );
+    //console.log(nodeId);
+    //console.log(this.props);
+    //console.log(this.state);
+  }
+
+  renderNodes(){
+    return _.map(this.props.pipelines.pipelinerunnodes, node => {
+      console.log(node);
+      return(
+        <div className="entry" key={node.id} onClick={()=>this.testClick(node.id)}>
+          {node.displayName}
+        </div>
+      );
+    });
+  }
+
   render(){
-    console.log(this.props);
+    //console.log(this.props);
     if(this.props.pipelines.pipelinerunnodes){
+
       return (
         <div className="container">
-          <Timeline nodes={this.props.pipelines.pipelinerunnodes}> </Timeline>
-          <Clpsb> </ Clpsb>
+            <div className="container">
+              <div className="bar"></div>
+              <div className="timeline">
+                <div className="startentry">
+                  Start
+                </div>
+                {this.renderNodes()}
+                <div className="endentry">
+                  End
+                </div>
+              </div>
+            </div>
+          <Clpsb pipelineName={this.state.pipelineName} runId={this.state.runId} nodeId={this.state.nodeId}> </ Clpsb>
         </div>
       );
     }
@@ -36,8 +81,6 @@ class PipelineTimeline extends Component {
   }
 
 }
-
-//export default PipelineTimeline;
 
 function mapStateToProps(state){
   return {pipelines: state.pipelines};
